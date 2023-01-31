d41ca4d6c2f987935db3980466aeae12486c4029
jainakshay1 <37770903+jainakshay1@users.noreply.github.com>
2023-02-01 02:46:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ async function uploadFileApi(token, content) {
                           
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
